94f1191f0989b3ba7f53d14a09ee410be79614d2
Albert Lin <Albertkuilin@gmail.com>
2020-08-03 03:00:35 -0400

    OCR implemented, but buggy with 1s

--- ValBot/extract.py
+++ ValBot/extract.py
@@ -2,6 +2,8 @@
 import cv2 
 import pytesseract 
 import os
+import numpy as np
+import random
 
 hsl_threshold_hue = [0.0, 180.0]
 hsl_threshold_saturation = [0.0, 149.82323232323233]
@@ -10,72 +12,132 @@ hsl_threshold_luminance = [169.69424460431657, 255.0]
 
 def extract(img_path):
 
-	data = {}
+	data = {} # key is in-game name, value is [team color, avg combat score, K, D, A, econ rating, 
+	          # first bloods, plants, defuses]
+
 	pytesseract.pytesseract.tesseract_cmd = os.environ.get('tesseract_path')
 
 	# Read image from which text needs to be extracted 
 	img = cv2.imread(img_path) 
 
-	# Preprocessing the image starts 
-
-	# Convert the image to HSL and filter
-	hsl = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-	hsl = cv2.inRange(hsl, (hsl_threshold_hue[0], hsl_threshold_luminance[0], hsl_threshold_saturation[0]),  (hsl_threshold_hue[1], hsl_threshold_luminance[1], hsl_threshold_saturation[1]))
-	cv2.imshow('hsl filter', hsl)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
-
-	# Performing OTSU threshold 
-	# ret, thresh1 = cv2.threshold(hsl, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
-
-	# Specify structure shape and kernel size. 
-	# Kernel size increases or decreases the area 
-	# of the rectangle to be detected. 
-	# A smaller value like (10, 10) will detect 
-	# each word instead of a sentence. 
-	rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 10)) 
+	# Convert the image to HSL and filter and close
+	processed = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+	processed = cv2.inRange(processed, (hsl_threshold_hue[0], hsl_threshold_luminance[0], hsl_threshold_saturation[0]),  (hsl_threshold_hue[1], hsl_threshold_luminance[1], hsl_threshold_saturation[1]))
+	kernel = np.ones((1,1),np.uint8)
+	processed = cv2.morphologyEx(processed, cv2.MORPH_CLOSE, kernel)
 
 	# Appplying dilation on the threshold image 
-	dilation = cv2.dilate(hsl, rect_kernel, iterations = 1) 
-	cv2.imshow('dilated', dilation)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+	rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 10)) 
+	dilation = cv2.dilate(processed, rect_kernel, iterations = 1) 
 
 	# Finding contours 
 	contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, 
 													cv2.CHAIN_APPROX_NONE) 
 	print(f'number of contours found: {len(contours)}')
-	for cnt in contours:
-		print(f'{cv2.boundingRect(cnt) }')
 
-	# Creating a copy of hsl image 
-	im2 = hsl.copy() 
-	imRects = hsl.copy()  # will draw rects on this
+	# Creating a copy of processed image 
+	imTess = processed.copy()      # will send to tesseract
+	imRects = img.copy()        # will draw rects on this
 
-	# Looping through the identified contours 
-	# Then rectangular part is cropped and passed on 
-	# to pytesseract for extracting text from it 
-	# Extracted text is then written into the text file 
-	annotation_color = (240, 255, 246)
+	# add player keys
+	annotation_color = (255, 255, 255)
+	for cnt in contours: 
+		text_color = (255, 255, 100)
+		x, y, w, h = cv2.boundingRect(cnt) 
+
+		# Apply OCR on the cropped image iff text is for player name
+		if x/float(imTess.shape[1]) < 0.2:
+			# Drawing a rectangle on copied image 
+			cv2.rectangle(imRects, (x, y), (x + w, y + h), annotation_color, 1)
+		
+			# Cropping the text block for giving input to OCR 
+			cropped = 255 - imTess[y:y + h, x:x + w]  # invert black and white
+			cropped = cv2.resize(cropped, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC) # enlarge
+			cropped = cv2.bilateralFilter(cropped,9,75,75) # blur
+			text = pytesseract.image_to_string(cropped, config=f"--psm 10")
+
+			# need to find team color too for player
+			cx, cy, cw, ch = x+w, y, int(0.1*img.shape[1]), h
+			color_region = img[cy:cy + ch, cx:cx + cw]
+			red = 0
+			blue = 0
+			color = ''
+			# randomly sample pixels
+			for i in range(100):
+				x_coord = random.randrange(0, color_region.shape[1])
+				y_coord = random.randrange(0, color_region.shape[0])
+				red += color_region[y_coord, x_coord, 2]
+				blue += color_region[y_coord, x_coord, 0]
+			if red > blue:
+				color = 'red'
+				cv2.putText(imRects, f'red', (cx, cy + int(ch*1.7)), cv2.FONT_HERSHEY_SIMPLEX, .4, text_color, 1)
+			else:
+				color = 'blue'
+				cv2.putText(imRects, f'blue', (cx, cy + int(ch*1.7)), cv2.FONT_HERSHEY_SIMPLEX, .4, text_color, 1)
+		
+			cv2.putText(imRects, f'{text}', (x, y + int(h*1.7)), cv2.FONT_HERSHEY_SIMPLEX, .4, text_color, 1)
+			data[text] = [int(y), color]
+
+	if len(data) != 10:
+		print(f'Number of players found ({len(data)}) does not equal 10! ERROR')
+		return [], None
+	print(data)
+		
+	# Looping through the data identified contours
 	i = 0
 	for cnt in contours: 
+		i += 1
+		text_color = (150, 255, 150)
 		x, y, w, h = cv2.boundingRect(cnt) 
+		if x/float(imTess.shape[1]) >= 0.2: # for stats, only detect as digits
+			# Drawing a rectangle on copied image 
+			cv2.rectangle(imRects, (x, y), (x + w, y + h), annotation_color, 1)
 		
-		# Drawing a rectangle on copied image 
-		cv2.rectangle(imRects, (x, y), (x + w, y + h), annotation_color, 3)
+			# Cropping the text block for giving input to OCR 
+			cropped = imTess[y:y + h, x:x + w]  
+			cropped = cv2.resize(cropped, None, fx=4, fy=4, interpolation=cv2.INTER_CUBIC) # enlarge
+			kernel = np.ones((2,2),np.uint8)
+			cropped = cv2.dilate(cropped,kernel,iterations = 2)
+			cropped = cv2.bilateralFilter(cropped,9,75,75) # blur
+			cropped = 255 - cropped  # invert black and white
+			cv2.imshow(f'{i}', cropped)
+			
+			# apply OCR on cropped image
+			text = pytesseract.image_to_string(cropped, config=f"--psm 13 -c tessedit_char_whitelist=0123456789") 
+			cv2.putText(imRects, f'{text}', (x, y + int(h*1.7)), cv2.FONT_HERSHEY_SIMPLEX, .4, text_color, 1)
 		
-		# Cropping the text block for giving input to OCR 
-		cropped = im2[y:y + h, x:x + w]
-		
-		# Apply OCR on the cropped image 
-		text = pytesseract.image_to_string(cropped) 
+			# Appending the data into closest player by y-coord
+			closest = None
+			minYDist = float('inf')
+			for player in data:
+				if abs(y - data[player][0]) < minYDist:
+					minYDist =  abs(y - data[player][0])
+					closest = player
 
-		cv2.putText(imRects, f'{i}. {text}', (x + h, y + h), cv2.FONT_HERSHEY_SIMPLEX, .5, annotation_color, 1.2)
-		
-		# Appending the text into file 
-		data[i] = text
-		i+=1
-	cv2.imshow('detected text', imRects)
+			data[closest].extend([x, text])
+	
+	print(data)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
-	return data
+
+	# reformat the [y, color, x1, stat1, x2, stat2,...] into ordered [color, stat1, stat2, ...]
+	for key, value in data.items():
+		if len(value) != 18:
+			print(f'Data not associated properly for {key}! ERROR')
+			return [], None
+		stats = data[key][2:]
+		xs = stats[0::2]
+		nums = stats[1::2]
+		indices = np.argsort(xs)
+		newStats = [None] * (len(nums) + 1)
+		newStats[0] = data[key][1]
+		for i in range(len(nums)):
+			newStats[indices[i]+1] = nums[i]
+		data[key] = newStats
+
+	print('FINAL DATA:')
+	print(data)
+	cv2.imshow(f'FINAL IMAGE', imRects)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+	return data, imRects
