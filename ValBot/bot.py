3dd78467a54509ed859fab9f8eaa9880390cdc2a
Albert Lin <Albertkuilin@gmail.com>
2020-08-03 21:41:38 -0400

    made new functions for interacting with history, made a data storage solution

--- ValBot/bot.py
+++ ValBot/bot.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 import extract
-from storage import writeRoster, getRoster, addRoster, leaveRoster, linkRoster, unlinkRoster
+from storage import writeRoster, getRoster, addRoster, leaveRoster, linkRoster, unlinkRoster, getHistory, writeHistory, saveMatch, getPlayerStats, getMatch, getMatches 
 import datetime
 import requests
 import cv2
@@ -37,20 +37,21 @@ async def leave(ctx):
 	player = ctx.author.name
 	left = leaveRoster(player)
 	if left:
-		await ctx.send(f"'{player}' has left ValBot's roster! :sob:")
+		await ctx.send(f"```\n'{player}' has left ValBot's roster! 😭\n```")
 	else:
-		await ctx.send(f"'{player}' is already not on ValBot's roster! :sob:")
+		await ctx.send(f"```\n'{player}' is already not on ValBot's roster! 😭\n```")
 
 @bot.command(name='roster', help='List roster')
 @commands.check(checkChannelActive)
 async def roster(ctx):
 	player_string = ''
 	roster = getRoster()
+	spacing = 36
 	i = 0
 	for player, riotIDs in roster.items():
 		i+=1
-		player_string += f"{i + 1}.   {player}:\t\t\t{riotIDs}\n"
-	await ctx.send(f"Roster ({len(roster)} players):\nName:\t\t\tRiotIDs\n{player_string}")
+		player_string += f"{i}.   {f'{player}:'.ljust(spacing)}{riotIDs}\n"
+	await ctx.send(f"```\nRoster ({len(roster)} players):\n{'Name:'.ljust(spacing)}     RiotIDs\n{player_string}\n```")
 
 @bot.command(name='link', help='Link Riot ID to your Discord account')
 @commands.check(checkChannelActive)
@@ -59,14 +60,14 @@ async def link(ctx, riotID=None):
 	addRoster(player)
 
 	if not riotID:
-		await ctx.send("Please retry, missing a riotID [  !link '<riotID>'  ]")
+		await ctx.send("```Please retry, missing a riotID [  !link '<riotID>'  ]```")
 		return
 
 	linked = linkRoster(player, riotID)
 	if linked:
-		await ctx.send(f"'{riotID}' has been linked to '{player}'!")
+		await ctx.send(f"```'{riotID}' has been linked to '{player}'!```")
 	else:
-		await ctx.send(f"'{riotID}' is already linked with '{player}' or another player!")
+		await ctx.send(f"```'{riotID}' is already linked with '{player}' or another player!```")
 
 @bot.command(name='unlink', help='Unlink Riot ID from your Discord account')
 @commands.check(checkChannelActive)
@@ -74,14 +75,14 @@ async def unlink(ctx, riotID=None):
 	player = ctx.author.name
 	addRoster(player)
 	if not riotID:
-		await ctx.send("Please retry, missing a riotID [  !unlink <riotID>  ]")
+		await ctx.send("```Please retry, missing a riotID [  !unlink <riotID>  ]```")
 		return
 		
 	unlinked = unlinkRoster(player, riotID)
 	if unlinked:
-		await ctx.send(f"'{riotID}' has been unlinked from {player}!")
+		await ctx.send(f"```'{riotID}' has been unlinked from {player}!```")
 	else:
-		await ctx.send(f"'{riotID}' is already unlinked from {player}!")
+		await ctx.send(f"```'{riotID}' is already unlinked from {player}!```")
 
 current_img = ''  # path to current img to handle
 @bot.command(name='upload', help='Upload post game image for OCR')
@@ -90,7 +91,7 @@ async def upload(ctx):
     global current_img
     time = datetime.datetime.now().strftime('%Y%m%d_%H%M')
     if len(ctx.message.attachments) == 0:
-    	await ctx.channel.send('Please attach an image!')
+    	await ctx.channel.send('```Please attach an image!```')
     	return
     attach = ctx.message.attachments[0]
     print(f'received upload: {attach.url}')
@@ -98,8 +99,8 @@ async def upload(ctx):
     with open(path_name, 'wb') as f:
         f.write(requests.get(attach.url).content)
     current_img = path_name
-    await ctx.channel.send('Image downloaded as "' + time + '.png"')
-    await ctx.channel.send('Either !process or !cancel...')
+    await ctx.channel.send('```Image downloaded as "' + time + '.png"```')
+    await ctx.channel.send('```Either !process or !cancel...```')
 
 @bot.command(name='process', help='Process img after !upload')
 @commands.check(checkChannelActive)
@@ -110,19 +111,18 @@ async def process(ctx):
 	detected_riotIDs = []
 
 	if current_img == '':
-		await ctx.channel.send('No img currently active, please !upload')
+		await ctx.channel.send('```No img currently active, please !upload```')
 		return
-	await ctx.channel.send('processing...')
+	await ctx.channel.send('```processing...```')
 	data, img, error = extract.extract(current_img)
 	if error:
-		await ctx.channel.send(f'{error}')
+		await ctx.channel.send(f'```{error}```')
 	post_path = os.path.join(POST_IMG_PATH, (current_img.split("/")[-1]).split(".")[0] + '_post.png')
 	cv2.imwrite(post_path, img)
-	current_img = ''
-	await ctx.channel.send('done! sending results...')
+	await ctx.channel.send('```done! sending results...```')
 	await ctx.channel.send(file=discord.File(post_path))
 	if error:
-		await ctx.channel.send('quitting because of error')
+		await ctx.channel.send('```quitting because of error```')
 
 	# get list of players from data who are on roster
 	roster = getRoster()
@@ -134,7 +134,7 @@ async def process(ctx):
 	detected_string = ''
 	for i in range(len(detected_rosterIDs)):
 		detected_string += f'{detected_rosterIDs[i]} as "{detected_riotIDs[i]}"\n'
-	await ctx.channel.send(f'roster members ({len(detected_rosterIDs)}) found:\n{detected_string}\n....................')
+	await ctx.channel.send(f'```roster members ({len(detected_rosterIDs)}) found:\n{detected_string}\n```')
 
 	# get user input to confirm, cancel, or edit
 	def check(message):
@@ -143,7 +143,7 @@ async def process(ctx):
 	attributes = ['color', 'score', 'K', 'D', 'A', 'econ', 'bloods', 'plants', 'defuses', 'name']
 	change_log = {} # formatted as nested dictionary, player key then attribute key
 	while user_input != 'confirm' and user_input != 'cancel':
-		await ctx.channel.send(f'RESPOND OPTIONS: [  confirm  ],\t [  cancel  ],\t [  edit <name>:<attribute>:<value>  ]\n\npossible names\n{list(data.keys())}\n\neditable attributes\n{attributes}')
+		await ctx.channel.send(f'```RESPOND OPTIONS: [  confirm  ],\t [  cancel  ],\t [  edit <name>:<attribute>:<value>  ]\n\npossible names\n{list(data.keys())}\n\neditable attributes\n{attributes}```')
 		user_input = str((await bot.wait_for('message', check=check)).content)
 
 		if user_input == 'confirm' or user_input == 'cancel':
@@ -153,10 +153,10 @@ async def process(ctx):
 			# store the change, print all existing changes
 			name, attribute, value = (user_input.split(' ')[1]).split(':')
 			if name not in data:
-				await ctx.channel.send(f'{name} not a valid name, please try again')
+				await ctx.channel.send(f'```{name} not a valid name, please try again```')
 				continue
 			if attribute not in attributes:
-				await ctx.channel.send(f'{attribute} not a valid attribute, please try again')
+				await ctx.channel.send(f'```{attribute} not a valid attribute, please try again```')
 				continue
 			if name not in change_log:
 				change_log[name] = {}
@@ -170,19 +170,19 @@ async def process(ctx):
 					else:
 						change_string += f'\t{attr}: from "{data[name][attributes.index(attr)]}" to "{value[attr]}"\n'
 
-			await ctx.channel.send(f'Changes to be made:\n{change_string}\n....................')
+			await ctx.channel.send(f'```Changes to be made:\n{change_string}\n```')
 			continue
 
 		else:
-			await ctx.channel.send('invalid option, please try again')
+			await ctx.channel.send('```invalid option, please try again```')
 	if user_input == 'cancel':
 		current_img = ''
-		await ctx.channel.send('cancelled')
+		await ctx.channel.send('```cancelled```')
 
 	elif user_input == 'confirm':
 		user_input = ''
 		while user_input != 'blue' and user_input != 'red':
-			await ctx.channel.send("Who won? <blue/red>")
+			await ctx.channel.send("```Who won? <blue/red>```")
 			user_input = str((await bot.wait_for('message', check=check)).content)
 		winner = user_input
 
@@ -207,10 +207,16 @@ async def process(ctx):
 			save_string += f'{rosterID}: {f"WON" if stats[0] == 1 else f"LOST"} {stats[1:]}\n'
 
 
-		await ctx.channel.send(f'SAVING:\n{save_string}')
-		await ctx.channel.send('FINISHED <NOT YET IMPLEMENTED>')
-
-
+		await ctx.channel.send(f'```SAVING:\n{save_string}```')
+		timeKey = (current_img.split("/")[-1]).split(".")[0]
+		reformatted = datetime.datetime.strptime(timeKey,'%Y%m%d_%H%M').strftime("%B %#d, %Y at %#I:%M")
+		print(f'saving {reformatted} data with timeKey {timeKey}')
+		saved = saveMatch(relevant_data, timeKey)
+		current_img = ''
+		if saved:
+			await ctx.channel.send('```FINISHED SAVING```')
+		else:
+			await ctx.channel.send('```could not save!! error with duplicate key?```')
 
 
 @bot.command(name='cancel', help='Cancel img after !upload')
@@ -218,56 +224,75 @@ async def process(ctx):
 async def cancel(ctx):
 	global current_img
 	if current_img == '':
-		await ctx.channel.send('No img currently active to cancel!')
+		await ctx.channel.send('```No img currently active to cancel!```')
 		return
 	current_img = ''
-	await ctx.channel.send('Img has been cancelled!')
+	await ctx.channel.send('```Img has been cancelled!```')
+
+@bot.command(name='history', help='View entire stored history')
+@commands.check(checkChannelActive)
+async def history(ctx):
+	history = getHistory()
+	await ctx.channel.send(f'```{history}```')
+
+@bot.command(name='matches', help='List stored matches')
+@commands.check(checkChannelActive)
+async def matches(ctx):
+	matches = getMatches()
+	string = ''
+	i = 0
+	for match in matches:
+		i+=1
+		reformatted = datetime.datetime.strptime(match,'%Y%m%d_%H%M').strftime("%B %#d, %Y at %#I:%M")
+		string += f"{i}. [{reformatted}]\n"
+	await ctx.channel.send(f'```Stored Matches:\n{string}```')
+
 
 whitelist = ['A_L__'] # people who can use admin commands
 @bot.command(name='admin', help='run commands as admin')
 @commands.check(checkChannelActive)
 async def admin(ctx, command=None, target=None, riotID=None):
 	if ctx.author.name not in whitelist:
-		await ctx.channel.send("Do 100 pushups a day and perhaps one day, you'll have this power as your own!")
+		await ctx.channel.send("```Do 100 pushups a day and perhaps one day, you'll have this power as your own!```")
 		return
 	if not command:
-		await ctx.channel.send("Need to specify a command! [  !admin <command> <parameters...>  ]")
+		await ctx.channel.send("```Need to specify a command! [  !admin <command> <parameters...>  ]```")
 		return
 	commands = ['link', 'unlink', 'leave']
 	if command not in commands:
-		await ctx.channel.send(f"{command} is not a valid command! Try: {commands}")
+		await ctx.channel.send(f"```{command} is not a valid command! Try: {commands}```")
 		return
 
 	if command == 'link':
 		if not target or not riotID:
-			await ctx.channel.send("Need to specify a target and/or riotID! [  !admin link <discordID> <riotID>  ]")
+			await ctx.channel.send("```Need to specify a target and/or riotID! [  !admin link <discordID> <riotID>  ]```")
 			return
 		addRoster(target)
 		linked = linkRoster(target, riotID)
 		if linked:
-			await ctx.send(f"'{riotID}' has been linked to '{target}'!")
+			await ctx.send(f"```'{riotID}' has been linked to '{target}'!```")
 		else:
-			await ctx.send(f"'{riotID}' is already linked with '{target}' or another player!")
+			await ctx.send(f"```'{riotID}' is already linked with '{target}' or another player!```")
 
 	if command == 'unlink':
 		if not target or not riotID:
-			await ctx.channel.send("Need to specify a target and/or riotID! [  !admin unlink <discordID> <riotID>  ]")
+			await ctx.channel.send("```Need to specify a target and/or riotID! [  !admin unlink <discordID> <riotID>  ]```")
 			return
 		addRoster(target)
 		unlinked = unlinkRoster(target, riotID)
 		if unlinked:
-			await ctx.send(f"'{riotID}' has been unlinked from {target}!")
+			await ctx.send(f"```'{riotID}' has been unlinked from {target}!```")
 		else:
-			await ctx.send(f"'{riotID}' is already unlinked from {target}!")
+			await ctx.send(f"```'{riotID}' is already unlinked from {target}!```")
 
 	if command == 'leave':
 		if not target:
-			await ctx.channel.send("Need to specify a target! [  !admin leave <discordID>  ]")
+			await ctx.channel.send("```Need to specify a target! [  !admin leave <discordID>  ]```")
 			return
 		left = leaveRoster(target)
 		if left:
-			await ctx.send(f"'{target}' has left ValBot's roster! :sob:")
+			await ctx.send(f"```'{target}' has left ValBot's roster! 😭```")
 		else:
-			await ctx.send(f"'{target}' is already not on ValBot's roster! :sob:")
+			await ctx.send(f"```'{target}' is already not on ValBot's roster! 😭```")
 
 bot.run(TOKEN)
--- ValBot/storage.py
+++ ValBot/storage.py
@@ -4,14 +4,14 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROSTER_PATH = os.path.join(BASE_DIR, 'ValBot/data/roster.txt')
-MATCH_HISTORY_PATH = os.path.join(BASE_DIR, 'ValBot/data/match_history.txt')
+HISTORY_PATH = os.path.join(BASE_DIR, 'ValBot/data/history.txt')
 
 # write a roster out
 def writeRoster(roster):
 	with open(ROSTER_PATH, 'wb') as file:
 		pickle.dump(roster, file)
 			
-# returns roster list
+# returns roster dict
 def getRoster():
 	try:
 		with open(ROSTER_PATH, 'rb') as file:
@@ -22,7 +22,7 @@ def getRoster():
 		print(f'error with unpickling "{ROSTER_PATH}", returning an empty roster')
 		return {}
 
-# add player to roster list
+# add player to roster dict
 def addRoster(player):
 	roster = getRoster()
 	if player not in roster:
@@ -31,7 +31,7 @@ def addRoster(player):
 		return True
 	return False
 	
-# remove player from roster list
+# remove player from roster dict
 def leaveRoster(player):
 	roster = getRoster()
 	if player in roster:
@@ -60,3 +60,62 @@ def unlinkRoster(player, riotID):
 	roster[player].remove(riotID)
 	writeRoster(roster)
 	return True
+
+# write match history out
+def writeHistory(history):
+	with open(HISTORY_PATH, 'wb') as file:
+		pickle.dump(history, file)
+			
+# returns history dict
+def getHistory():
+	try:
+		with open(HISTORY_PATH, 'rb') as file:
+			return pickle.load(file)
+
+	except Exception as e:
+		print(e)
+		print(f'error with unpickling "{HISTORY_PATH}", returning an empty history')
+		return {}
+
+# return dictionary with keys: rosterID, val: list of stats
+def getMatch(timeKey):
+	history = getHistory()
+	matchDict = {}
+	found = False
+	for rosterID, dictOfMatches in history.items():
+		for time, stats in dictOfMatches.items():
+			if time == timeKey:
+				found = True
+				matchDict[rosterID] = stats
+				continue
+	if not found:
+		return None
+	return matchDict
+
+# return dictionary with keys: time, val: list of stats
+def getPlayerStats(rosterID):
+	history = getHistory()
+	if rosterID not in history:
+		return None
+	return history[rosterID]
+
+# save new match using a timeKey
+def saveMatch(data, timeKey):
+	history = getHistory()
+	for rosterID, dictOfMatches in history.items():
+		if timeKey in dictOfMatches:
+			return False
+	for player, stats in data.items():
+		if player not in history:
+			history[player] = {}
+		history[player][timeKey] = stats
+	writeHistory(history)
+	return True
+
+# get list of stored matches
+def getMatches():
+	history = getHistory()
+	matches = []
+	for rosterID, matchDict in history.items():
+		matches.extend(matchDict.keys())
+	return set(matches)
